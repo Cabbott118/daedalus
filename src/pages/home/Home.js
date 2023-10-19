648db51c6f1fe54d92b5178cbc7d3b1ea1bb3fa7
unknown <calebhaabbott94@gmail.com>
2023-10-19 19:38:46 -0400

    Latest changes on home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 // Components
 import Loader from 'components/common/Loader';
 import Hero from 'pages/home/components/Hero';
-// import EnrollmentBanner from 'pages/home/components/EnrollmentBanner';
 import ServiceTicketTabs from 'pages/home/components/serviceTicketList/ServiceTicketTabs';
 import TicketCounter from 'pages/home/components/TicketCounter';
 import WelcomeBanner from 'pages/home/components/WelcomeBanner';
@@ -45,7 +44,7 @@ export default function Home() {
   const { serviceTickets, loading: serviceTicketsLoading } = useSelector(
     (state) => state.serviceTicket
   );
-  console.log(serviceTicketsLoading);
+
   const { data: administratorData, loading: administratorLoading } =
     useSelector((state) => state.administrator);
 
@@ -73,6 +72,7 @@ export default function Home() {
   useEffect(() => {
     if (userData?.userType === UserType.ADMINISTRATOR) {
       dispatch(fetchAdministrator(userData?.uid));
+      dispatch(fetchServiceTicketsAssignedToProvider(administratorData?.uid));
     } else if (userData?.userType === UserType.CONTRACTOR) {
       dispatch(fetchContractorByContactId(userData?.uid));
     } else if (userData?.userType === UserType.CUSTOMER) {
@@ -94,66 +94,22 @@ export default function Home() {
     }
   }, [
     userData,
-    dispatch,
     administratorData,
     contractorData,
     customerData,
     technicianData,
+    dispatch,
   ]);
-
-  const colors = [
-    theme.palette.primary.light,
-    theme.palette.secondary.main,
-    'white',
-  ];
-
-  // if (
-  //   userLoading ||
-  //   administratorLoading ||
-  //   contractorLoading ||
-  //   customerLoading ||
-  //   technicianLoading
-  // )
-  //   return <Loader style={{ fill: theme.palette.primary.main }} />;
 
   if (!userData)
     return (
       <>
         <Hero />
-        {/* <EnrollmentBanner /> */}
       </>
     );
 
   return (
     <>
-      {/* <Box
-        sx={{
-          height: 100,
-          bgcolor: theme.palette.primary.dark,
-          position: 'relative',
-          overflow: 'hidden',
-        }}
-      >
-        {[...Array(30)].map((_, index) => {
-          const size = Math.random() * 10 + 2; // Random size between 5px and 25px
-
-          return (
-            <Box
-              key={index}
-              style={{
-                position: 'absolute',
-                width: `${size}px`, // Symmetrical width
-                height: `${size}px`, // Symmetrical height
-                backgroundColor: colors[index % colors.length], // Cycle through the colors array
-                borderRadius: '50%',
-                top: `${Math.random() * 80}px`, // Random top position
-                left: `${Math.random() * 100}%`, // Random left position
-              }}
-            ></Box>
-          );
-        })}
-      </Box> */}
-
       <Container maxWidth='md' sx={{ mt: 3 }}>
         {userData && (
           <>
@@ -161,23 +117,6 @@ export default function Home() {
               <Grid item xs={12}>
                 <WelcomeBanner userData={userData} theme={theme} />
               </Grid>
-              {/* <Grid item xs={12}>
-                {administratorData && (
-                  <Typography variant='subtitle1' component='h1'>
-                    {administratorData?.name} - Daedalus
-                  </Typography>
-                )}
-                {contractorData && (
-                  <Typography variant='subtitle1' component='h1'>
-                    {contractorData?.name} - Daedalus
-                  </Typography>
-                )}
-                {customerData && (
-                  <Typography variant='subtitle1' component='h1'>
-                    {customerData?.name} - Daedalus
-                  </Typography>
-                )}
-              </Grid> */}
             </Grid>
             <Grid container spacing={3} sx={{ mt: 5 }}>
               <Grid item xs={12} md={4}>
